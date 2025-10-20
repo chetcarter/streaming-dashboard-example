4150ce6e097f223caa425056e82ad07b5c5c3655
Chet Carter <abazabalv@gmail.com>
2025-10-20 10:49:25 -0700

    adds tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-
-const Card = ({ title }) => {
-	const [hasLiked, setHasLiked] = useState({ initialState: false });
-
-	return (
-		<div className='card'>
-			<h2>{title}</h2>
-
-			<button onClick={() => setHasLiked(!hasLiked)}>
-			{hasLiked ? '❤️' : '🤍'}
-			</button>
-		</div>
-	);
-};
+import React from 'react'
 
 const App = () => {
 	return (
-		<div className='card-container'>
-			<Card title='Star Wars' />
-			<Card title='Avatar' />
-			<Card title='The Lion King' />
-		</div>
-	);
-};
+		<div>App</div>
+	)
+}
 
-export default App;
+export default App
